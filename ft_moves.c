e3d45f16934ee65b15c42a9f97ed5afa46340e99
Patricia Alexandra Cesar Monteiro <palexand@c1r5s9.42porto.com>
2024-12-03 18:51:34 +0000

    Added moves and sorting

--- ft_moves.c
+++ ft_moves.c
@@ -2,7 +2,7 @@
 // create a function that swaps the first two elements of a stack;
 // do nothing if there is only one or no elements.
 // not allowed to use -> prev
-void	ft_swap(t_stack *stack)
+void	ft_swap(t_stack *stack, char c)
 {
 	t_stack	*temp;
 	t_stack	*head;
@@ -16,19 +16,31 @@ void	ft_swap(t_stack *stack)
 		temp->next = head;
 		head = temp;
 	}
+	if (c == 'a')
+	{
+		ft_printf("sa\n");
+	}
+	else if (c == 'b')
+	{
+		ft_printf("sb\n");
+	}
+	else if (c == 's')
+	{
+		ft_printf("ss\n");
+	}
 }
 
 void ft_ss(t_stack *stack_a, t_stack *stack_b)
 {
-	ft_swap(stack_a);
-	ft_swap(stack_b);
+	ft_swap(stack_a, 's');
+	ft_swap(stack_b, ' ');
 }
 
 // function that pushes the first element of a stack to the other stack;
 // do nothing if the stack is empty.
 // not allowed to use -> prev
 
-void ft_push(t_stack *stack_a, t_stack *stack_b)
+void ft_push(t_stack *stack_a, t_stack *stack_b, char c)
 {
 	t_stack	*temp;
 	t_stack	*head_a;
@@ -44,13 +56,21 @@ void ft_push(t_stack *stack_a, t_stack *stack_b)
 		head_a = temp;
 		stack_a = temp;
 	}
+	if (c == 'a')
+	{
+		ft_printf("pa\n");
+	}
+	else if (c == 'b')
+	{
+		ft_printf("pb\n");
+	}
 }
 
 // function that rotates the stack by shifting up all elements by 1;
 // the first element becomes the last one.
 // not allowed to use -> prev
 
-void ft_rotate(t_stack *stack)
+void ft_rotate(t_stack *stack, char c)
 {
 	t_stack	*temp;
 	t_stack	*head;
@@ -65,19 +85,31 @@ void ft_rotate(t_stack *stack)
 		temp->next = head;
 		head->next = NULL;
 	}
+	if (c == 'a')
+	{
+		ft_printf("ra\n");
+	}
+	else if (c == 'b')
+	{
+		ft_printf("rb\n");
+	}
+	else if (c == 'r')
+	{
+		ft_printf("rr\n");
+	}
 }
 
 void ft_rr(t_stack *stack_a, t_stack *stack_b)
 {
-	ft_rotate(stack_a);
-	ft_rotate(stack_b);
+	ft_rotate(stack_a, 'r');
+	ft_rotate(stack_b, ' ');
 }
 
 // function that rotates the stack by shifting down all elements by 1; 
 // the last element becomes the first one.
 // not allowed to use -> prev
 
-void ft_reverse_rotate(t_stack *stack)
+void ft_reverse_rotate(t_stack *stack, char c)
 {
 	t_stack	*temp;
 	t_stack	*head;
@@ -94,10 +126,19 @@ void ft_reverse_rotate(t_stack *stack)
 		temp->next = head;
 		head->next = NULL;
 	}
+	if (c == 'a')
+	{
+		ft_printf("rra\n");
+	}
+	else if (c == 'b'){
+		ft_printf("rrb\n");
+	}
+	else if (c == 'r'){
+		ft_printf("rrr\n");
+	}
 }
-
 void ft_rrr(t_stack *stack_a, t_stack *stack_b)
 {
-	ft_reverse_rotate(stack_a);
-	ft_reverse_rotate(stack_b);
+	ft_reverse_rotate(stack_a, 'r');
+	ft_reverse_rotate(stack_b, ' ');
 }
--- sorted.c
+++ sorted.c
@@ -28,3 +28,83 @@ int len(t_stack *stack)
 	}
 	return (i);
 }
+
+t_stack *get_last(t_stack *stack)
+{
+	t_stack *temp;
+	t_stack *smallest;
+	temp = stack;
+	while (temp->next)
+	{
+		if (temp->content < temp->next->content)
+			smallest = temp;
+		temp = temp->next;
+	}
+	return (smallest);
+}
+
+void *sort_3(t_stack *a)
+{
+	int top;
+	int middle;
+	int bottom;
+
+	top = a->content;
+	middle = a->next->content;
+	bottom = a->next->next->content;
+
+	if (top > bottom && bottom > middle)
+		ft_rotate(a, 'a');
+	else if (top > middle && middle > bottom)
+	{
+	ft_swap(a, 'a');
+	ft_reverse_rotate(a, 'a');
+	}
+	else if (middle > top && top > bottom)
+		ft_reverse_rotate(a, 'a');
+	else if (middle > bottom && bottom > top)
+		ft_rotate(a, 'a');
+	else if (bottom > top && top > middle)
+		ft_swap(a, 'a');
+}
+
+void	*sort_4(t_stack *a, t_stack *b, int i)
+{
+	if (a->next->next->next->content == (get_last(a))->content)
+	{
+		while (a->content != (get_last(a))->content)
+			ft_reverse_rotate(a, 'a');
+	}
+	else
+	{
+		while (a->content != (get_last(a))->content)
+			ft_rotate(a, 'a');
+	}
+	if (!sorted(a))
+	{
+		ft_push(b, a, 'b');
+		sort_3(a);
+		ft_push(a, b, 'a');
+	}
+}
+
+void *sort_5(t_stack *a, t_stack *b, int i)
+{
+	if (a->next->next->next->next->content == (get_last(a))->content
+		|| a->next->next->next->content == (get_last(a))->content)
+	{
+		while (a->content != (get_last(a))->content)
+			ft_reverse_rotate(a, 'a');
+	}
+	else
+	{
+		while (a->content != (get_last(a))->content)
+			ft_rotate(a, 'a');
+	}
+	if (!sorted(a))
+	{
+		ft_push(b, a, 'b');
+		sort_4(a, b, i);
+		ft_push(a, b, 'a');
+	}
+}
